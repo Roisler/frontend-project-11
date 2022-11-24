865de17d2c50d9f472ad1b5a0bdda01a3b75dcf2
roisler <roislerxs@gmail.com>
2022-11-24 18:26:27 +0700

    fix remarks

--- src/app.js
+++ src/app.js
@@ -1,32 +1,193 @@
 import 'bootstrap';
+import _ from 'lodash';
+import axios from 'axios';
+import * as yup from 'yup';
 import onChange from 'on-change';
-import { formSubmit, modalClick } from './handlers';
-import render from './view';
-import { updatePosts } from './getData';
+import i18n from 'i18next';
+import watch from './view';
+import ru from './locales/ru';
+import parser from './parser';
 import './styles.scss';
 
-export default () => {
-  const initialState = {
-    rssForm: {
-      status: '',
-      buttonDisabled: false,
-    },
-    uiState: [],
-    data: {
-      currentUrl: '',
-      urls: [],
-      feeds: [],
-      posts: [],
-      errors: {},
-    },
-  };
+const route = (url) => {
+  const resultUrl = new URL('https://allorigins.hexlet.app/get');
+  resultUrl.searchParams.set('disableCache', true);
+  resultUrl.searchParams.set('url', url);
+  return resultUrl;
+};
 
-  const state = onChange(initialState, (path, value) => {
-    render(initialState, path, value);
+const validate = (data) => {
+  yup.setLocale({
+    mixed: {
+      notOneOf: 'errors.already_exists',
+    },
+    string: {
+      url: 'errors.invalid_url',
+      required: 'errors.required_field',
+    },
   });
 
-  setInterval(() => updatePosts(state), 5000);
+  const schema = yup.object().shape({
+    currentUrl: yup.string()
+      .notOneOf([yup.ref('urls')])
+      .url()
+      .required(),
+    urls: yup.array(),
+    feeds: yup.array(),
+    posts: yup.array(),
+  });
+  return schema.validate(data);
+};
 
-  formSubmit(document, state);
-  modalClick(document, state);
+const extractPosts = (data, state, feedId) => {
+  const newData = _.cloneDeep(data);
+  const { posts } = newData;
+  posts.forEach((el) => {
+    const post = el;
+    const currentPost = _.find(state.data.posts, (elem) => elem.link === post.link);
+    if (currentPost) {
+      return;
+    }
+    post.id = _.uniqueId('post_');
+    post.feedId = feedId;
+    state.uiState.posts.push({ id: post.id, viewed: false });
+    state.data.posts.push(post);
+  });
+};
+
+const extractFeeds = (data, state) => {
+  const newData = _.cloneDeep(data);
+  const { feed } = newData;
+  // проверяем, нет ли уже такого feed
+  const currentFeed = _.find(state.data.feeds, (el) => el.title === feed.title);
+  if (currentFeed) {
+    extractPosts(data, state, currentFeed.id);
+  } else {
+    feed.id = state.data.feeds.length + 1;
+    state.data.feeds.push(feed);
+    extractPosts(data, state, feed.id);
+  }
+};
+
+const updatePosts = (state) => {
+  const { urls } = state.data;
+  if (urls.length === 0) {
+    setTimeout(() => updatePosts(state), 5000);
+    return;
+  }
+  Promise.all(urls.map((url) => axios.get(route(url)).then((response) => {
+    const data = parser(response.data.contents);
+    extractFeeds(data, state);
+  }))).then(() => setTimeout(() => updatePosts(state), 5000))
+    .catch(() => setTimeout(() => updatePosts(state), 5000));
+};
+
+const getData = (state, path, e) => {
+  const initState = state;
+  axios.get(route(path)).then((response) => {
+    const data = parser(response.data.contents);
+    initState.data.urls.push(path);
+    extractFeeds(data, state);
+    initState.data.errors = {};
+    initState.rssForm.status = 'valid';
+    initState.rssForm.buttonDisabled = false;
+    e.target.reset();
+  }).catch((error) => {
+    initState.data.errors = error;
+    initState.rssForm.status = 'invalid';
+    initState.rssForm.buttonDisabled = false;
+  });
+};
+
+const modalClick = (document, state) => {
+  const { uiState } = state;
+  const { posts } = state.data;
+
+  const modal = document.querySelector('#modal');
+  modal.addEventListener('show.bs.modal', (e) => {
+    const modalButton = e.relatedTarget;
+    const { id } = modalButton.dataset;
+
+    const selectedPost = _.find(posts, (post) => post.id === id);
+
+    uiState.modal = selectedPost.id;
+
+    uiState.posts.forEach((element) => {
+      const post = element;
+      if (post.id === id) {
+        post.viewed = true;
+      }
+    });
+  });
+};
+
+const formSubmit = (document, state) => {
+  const input = document.querySelector('#url-input');
+  const form = document.querySelector('.rss-form');
+  const initState = state;
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    initState.rssForm.status = 'process';
+    initState.rssForm.buttonDisabled = true;
+    const formData = new FormData(form);
+    const currentUrl = formData.get('url');
+    initState.data.currentUrl = currentUrl;
+    validate(state.data)
+      .then(() => {
+        getData(state, currentUrl, e);
+      })
+      .catch((err) => {
+        initState.data.errors = err;
+        initState.rssForm.status = 'invalid';
+        initState.rssForm.buttonDisabled = false;
+      });
+    input.focus();
+  });
+};
+
+export default () => {
+  i18n.init({
+    lng: 'ru',
+    resources: {
+      ru,
+    },
+  }).then(() => {
+    const app = () => {
+      const initialState = {
+        rssForm: {
+          status: '',
+          buttonDisabled: false,
+        },
+        uiState: {
+          modal: '',
+          posts: [],
+        },
+        data: {
+          currentUrl: '',
+          urls: [],
+          feeds: [],
+          posts: [],
+          errors: {},
+        },
+      };
+
+      const newElements = (item) => {
+        const elements = {
+          container: document.querySelector(`.${item}`),
+          card: document.createElement('div'),
+          cardBody: document.createElement('div'),
+          header: document.createElement('h2'),
+        };
+        return elements;
+      };
+
+      const state = onChange(initialState, (path, value) => {
+        watch(initialState, path, value, newElements);
+      });
+      setTimeout(() => updatePosts(state), 5000);
+      formSubmit(document, state);
+      modalClick(document, state);
+    };
+    app(i18n);
+  });
 };
--- src/view.js
+++ src/view.js
@@ -1,21 +1,144 @@
-import renderForm from './renderForm';
-import renderPosts from './renderPosts';
-import renderFeeds from './renderFeeds';
-import renderErrors from './renderErrors';
+import i18n from 'i18next';
+import _ from 'lodash';
 
-export default (state, path, value) => {
+const getPostUi = (state, id) => {
+  const uiPosts = state.uiState.posts;
+  const statePostView = _.find(uiPosts, (uiPost) => uiPost.id === id);
+  return statePostView.viewed;
+};
+
+const renderCards = (item, itemList, newElements) => {
+  const elements = newElements(item);
+
+  elements.card.classList.add('card', 'border-0');
+  elements.cardBody.classList.add('card-body');
+  elements.header.classList.add('card-title', 'h4');
+  elements.header.textContent = i18n.t(`cards.${item}`);
+  elements.cardBody.append(elements.header);
+  elements.card.append(elements.cardBody);
+  elements.container.replaceChildren(elements.card);
+  elements.card.append(itemList);
+};
+
+const renderPosts = (state, newElements) => {
+  const { posts } = state.data;
+
+  const postsList = document.createElement('ul');
+  postsList.classList.add('list-group', 'border-0', 'rounded-0');
+
+  posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start');
+    li.classList.add('border-0', 'border-end-0');
+    const link = document.createElement('a');
+    link.setAttribute('href', post.link);
+    link.dataset.id = post.id;
+    link.setAttribute('rel', 'noopener noreferrer');
+    link.setAttribute('target', '_blank');
+    const postView = getPostUi(state, post.id);
+    if (postView === false) {
+      link.classList.add('fw-bold');
+    } else {
+      link.classList.add('fw-normal', 'link-secondary');
+    }
+    link.textContent = post.title;
+
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-bs-target', '#modal');
+    button.dataset.id = post.id;
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.textContent = i18n.t('view');
+
+    li.append(link, button);
+    postsList.prepend(li);
+  });
+  renderCards('posts', postsList, newElements);
+};
+
+const renderFeeds = (state, newElements) => {
+  const { feeds } = state.data;
+
+  const feedsList = document.createElement('ul');
+  feedsList.classList.add('list-group', 'border-0', 'rounded-0');
+
+  feeds.forEach((feed) => {
+    const elements = {
+      li: document.createElement('li'),
+      title: document.createElement('h3'),
+      description: document.createElement('p'),
+    };
+    elements.li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    elements.title.classList.add('h6', 'm-0');
+    elements.title.textContent = feed.title;
+    elements.description.classList.add('m-0', 'small', 'text-black-50');
+    elements.description.textContent = feed.description;
+
+    elements.li.append(elements.title, elements.description);
+    feedsList.prepend(elements.li);
+  });
+  renderCards('feeds', feedsList, newElements);
+};
+
+const renderForm = (value) => {
+  const form = document.querySelector('form');
+  const submit = form.querySelector('[type="submit"]');
+  if (value === true) {
+    submit.setAttribute('disabled', 'disabled');
+  } else {
+    submit.disabled = false;
+  }
+};
+
+const renderErrors = (errors, value) => {
+  const feedback = document.querySelector('.feedback');
+  const input = document.querySelector('#url-input');
+  if (value === 'valid') {
+    feedback.textContent = i18n.t('success_load');
+    input.classList.remove('is-invalid');
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
+  } else if (value === 'process') {
+    feedback.textContent = '';
+    input.classList.remove('is-invalid');
+  } else {
+    input.classList.add('is-invalid');
+    feedback.classList.add('text-danger');
+    feedback.textContent = errors.message === 'Network Error' ? i18n.t('errors.network_error') : i18n.t(errors.message);
+  }
+};
+
+const renderModal = (state, id) => {
+  const { posts } = state.data;
+  const currentPost = _.find(posts, (post) => post.id === id);
+  const modal = document.querySelector('#modal');
+  const modalTitle = modal.querySelector('.modal-title');
+  modalTitle.textContent = currentPost.title;
+
+  const modalBody = modal.querySelector('.modal-body');
+  modalBody.textContent = currentPost.description;
+
+  const buttonFull = modal.querySelector('.full-article');
+  buttonFull.setAttribute('href', currentPost.link);
+};
+
+export default (state, path, value, newElements) => {
   switch (path) {
-    case 'uiState' || 'data.posts':
-      renderPosts(state, 'posts');
+    case 'uiState.posts' || 'data.posts':
+      renderPosts(state, newElements);
       break;
     case 'rssForm.buttonDisabled':
       renderForm(value);
       break;
+    case 'uiState.modal':
+      renderModal(state, value);
+      break;
     case 'rssForm.status':
       if (value === 'valid') {
         renderErrors(state.data.errors, value);
-        renderPosts(state, 'posts');
-        renderFeeds(state, 'feeds');
+        renderFeeds(state, newElements);
+        renderPosts(state, newElements);
       } else {
         renderErrors(state.data.errors, value);
       }
